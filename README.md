a3b173b94f3d8405379baf305c9dbf08393ce57a
Art 3870-02 <art3870-02@Arts-194-108.local>
2018-04-18 16:10:01 -0700

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+# portfolio
+<b> portfolio for ligman's artwork </b>
+
+## resources
+Markdown Cheatsheet - https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
+Foundation Cheatsheets - https://github.com/csueastbay/art3870
+
+## inspiration
+
+## other stuff
---
+++ js/add-content.js
@@ -0,0 +1,16 @@
+// Greetings per time of day
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {//6:00 PM
+  greeting = 'Good evening!';
+} else if (hourNow > 12) { //12:00 PM
+  greeting = 'Good afternoon!';
+} else if (hourNow > 0) { //12:00 Midnight
+  greeting = 'Good morning!';
+} else {
+  greeting = 'Welcome!';
+}
+document.write('<h3>' + greeting + '</h3>');
